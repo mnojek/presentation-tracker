a606bf9c4083b827fa8022a7f19481ee1992ef52
Mateusz Nojek <matnojek@gmail.com>
2020-05-02 18:43:09 +0200

    Working progress bar for modules
    Application loads JSON presentation data and displays it as progress bar.
    Data contains title, subtitle and time of presentation and subsequent modules.
    Progress bar has indicator that shows present part of presentation depending
    on the duration of modules.

---
+++ presentation.js
@@ -0,0 +1,41 @@
+var data = 
+{
+	"presentation": {
+		"title": "Robot Framework",
+		"subtitle": "Dzień 1",
+		"time": 5,
+		"modules": [
+			{
+				"label": "Moduł 1",
+				"time": 1,
+				"color": "green",
+				"type": "module"
+			},
+			{
+				"label": "Moduł 2",
+				"time": 2,
+				"color": "red",
+				"type": "module"
+			},
+			{
+				"label": "Przerwa",
+				"time": 1,
+				"color": "gray",
+				"type": "break"
+			},
+			{
+				"label": "Moduł 3",
+				"time": 1,
+				"color": "blue",
+				"type": "module"
+			}
+		]
+	},
+	"colors": [
+		"#007bff",
+		"#17a2b8",
+		"#28a745",
+		"#ffc107",
+		"#dc3545"
+	]
+}
